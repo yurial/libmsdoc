8b50884a950c6bd48a4c7f960a1984c835ed319a
Yuri Dyachenko <libmsdoc@yurial.ru>
2011-10-24 22:17:18 +0400

    add: CT_SheetViews

---
+++ src/spreadsheetml/ct_sheetviews.h
@@ -0,0 +1,16 @@
+#ifndef CT_SHEETVIEWSH
+#define CT_SHEETVIEWSH
+
+namespace shpreadsheetml
+{
+
+struct CT_SheetViews
+{
+//TODO:CT_Extension    extLst;      //$18.2.10 (Future Feature Data Storage Area)
+//TODO:CT_SheetView    sheetView[]; //$18.3.1.87 (Worksheet View)
+};
+
+}//namespace spreasheetml
+
+#endif
+
