4a9846276bf2e77bc55868dedba40c744294122d
Yuri Dyachenko <libmsdoc@yurial.ru>
2011-10-24 20:48:52 +0400

    add: CT_Sheets

---
+++ src/spreadsheetml/ct_sheets.h
@@ -0,0 +1,16 @@
+#ifndef CT_SHEETSH
+#define CT_SHEETSH
+
+#include "ct_sheet.h"
+
+namespace spreadsheetml
+{
+//Tsheets   $18.2.20 (Sheets)
+//Tsheet    $18.2.19 (Sheet Information)
+typedef std::list<CT_Sheet> CT_Sheets;
+};
+
+}//namespace spreadsheetml
+
+#endif
+
