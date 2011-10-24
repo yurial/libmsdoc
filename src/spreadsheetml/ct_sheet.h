7ebf0025991a650680490776bcfb4bea285aef69
Yuri Dyachenko <libmsdoc@yurial.ru>
2011-10-24 20:48:41 +0400

    add: CT_Sheet

---
+++ src/spreadsheetml/ct_sheet.h
@@ -0,0 +1,20 @@
+#ifndef CT_SHEETH
+#define CT_SHEETH
+
+namespace spreadsheetml
+{
+//sheet $18.2.19 (Sheet Information)
+struct CT_Sheet
+{
+//attributes
+//TODO:ST_RelationshipId   id;         //$22.8.2.1  (Relationship Id)
+//TODO:ST_Xstring          name;       //$22.9.2.19 (Sheet Name)
+//TODO:int                 sheetId;    //(Sheet Tab Id)
+//TODO:ST_SheetState       state;      //$18.18.68  (Visible State)
+//childs
+};
+
+}//namespace spreadsheetml
+
+#endif
+
